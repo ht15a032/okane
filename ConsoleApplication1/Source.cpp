f01831ccfed9991e1b3a64b148f6e6f7e716a165
unknown <>
2017-04-14 15:57:54 +0900

    Change: コンテナを用いたループ処理の学習

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	cout << "Hello World!" << endl;
+	vector<int> count;
+
+	for(auto i=0; i<100; i++) {
+		count.push_back(i);
+	}
+
+	for(auto i : count ) {
+		cout << "Count: " << i << endl;
+	}
 
 	return 0;
 
